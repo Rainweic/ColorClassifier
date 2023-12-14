1814639a39de0d621b13f098be56bac551e49463
Rainweic <861514706@qq.com>
2023-12-14 14:30:36 +0800

    [test]

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod model;
 
 fn main() {
 
+    // For test yolov8.py
     let mut yolo_module = model::DetModule::new();
     yolo_module.load_model("yolov8n.pt");
     let res = yolo_module.infer("/Users/new/Downloads/car.png");
@@ -12,4 +13,5 @@ fn main() {
         None => {}
         Some(res) => println!("res: {:?}", res)
     };
+
 }
