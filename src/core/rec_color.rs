f85ba635fe36f14934cd09cc78099630f8f8a9a2
cu <861514706@qq.com>
2023-12-25 17:16:51 +0800

    [update]

--- src/core/rec_color.rs
+++ src/core/rec_color.rs
@@ -1,6 +1,7 @@
-use opencv::core::{Mat, Rect, Vector};
-use opencv::imgcodecs::{imread, imwrite, IMREAD_COLOR};
-use opencv::imgproc::{cvt_color, COLOR_BGR2GRAY, threshold, THRESH_BINARY, THRESH_OTSU};
+use opencv::core::{Mat, Rect, Point, Vec3b};
+use opencv::hub_prelude::MatTraitConst;
+use opencv::imgcodecs::{imread, IMREAD_COLOR};
+use opencv::imgproc::{cvt_color, COLOR_BGR2GRAY, threshold, THRESH_BINARY, THRESH_OTSU, COLOR_BGR2HSV};
 
 use crate::model::{DetModule, DetResult};
 
@@ -9,7 +10,7 @@ use crate::model::{DetModule, DetResult};
 /// Step2 做区域分割
 /// Step3 做颜色提取
 #[allow(dead_code)]
-pub fn det_color(yolo: DetModule, img_path: &str) {
+pub fn color_extraction(yolo: DetModule, img_path: &str) {
 
     let det_res_vec: Vec<DetResult> = yolo.infer(img_path);
     let img = imread(img_path, IMREAD_COLOR);
@@ -20,17 +21,11 @@ pub fn det_color(yolo: DetModule, img_path: &str) {
             // 提取ROI图像
             let img_roi_vec = extract_roi(det_res_vec, img);
 
-            // Test
-            // for (i, _img) in img_roi_vec.iter().enumerate() {
-            //     let img_name = format!("img_{:?}.jpg", i);
-            //     let params = Vector::new();
-            //     imwrite(&img_name, _img, &params).expect("写入图像失败");
-            // }
-
             // 对ROI区域做边缘检测
-            let binnary_img_vec = segmentation(img_roi_vec);
+            let img_seg_vec = segmentation(img_roi_vec);
 
             // 颜色提取
+            cal_mean_value_of_hsv(img_seg_vec);
         },
         Err(e) => {
             println!("读取图片错误: {:?}", e.to_string());
@@ -65,26 +60,77 @@ pub fn segmentation(img_roi_vec: Vec<Mat>) -> Vec<Mat> {
         let mut gray_img: Mat = Mat::default();
         cvt_color(img, &mut gray_img, COLOR_BGR2GRAY, 0).expect("BRG2GRAY失败");
 
-        // TEST
-        // let params = Vector::new();
-        // imwrite("gray_img.jpg", &gray_img, &params);
-
         // Otsu二值化
-        let mut binary_img = Mat::default();
+        let mut mask = Mat::default();
         threshold(
             &gray_img, 
-            &mut binary_img, 
+            &mut mask, 
             0.0, 
             255.0, 
             THRESH_BINARY + THRESH_OTSU
         ).expect("二值化失败");
 
-        // TEST
-        // let params = Vector::new();
-        // imwrite("binary_img.jpg", &binary_img, &params);
+        // 利用二值化图像当作mask 从原图中提取区域图像
+        let mut seg_img = Mat::default();
+        img.copy_to_masked(&mut seg_img, &mask).expect("copy_to_masked失败");
 
-        ret.push(binary_img);
+        ret.push(seg_img);
     }
 
     ret
+}
+
+/// 计算HSV均值
+pub fn cal_mean_value_of_hsv(binnary_img_vec: Vec<Mat>) -> Vec<(i32, i32, i32)> {
+
+    let mut mean_hsv_vec = vec![];
+
+    for img in binnary_img_vec.iter() {
+
+        // BGR 2 HSV
+        let mut img_hsv: Mat = Mat::default();
+        cvt_color(img, &mut img_hsv, COLOR_BGR2HSV, 0)
+            .expect("BGR2HSV失败");
+
+        // 统计有效像素点均值
+        let unvalid_hsv_value = [0, 0, 0];
+        let mut n_valid_pt: i32 = 0;
+        let mut h_value_sum: i32 = 0;
+        let mut s_value_sum: i32 = 0;
+        let mut v_value_sum: i32 = 0;
+
+        // 遍历像素点
+        let row_number = img_hsv.rows();
+        let col_number = img_hsv.cols();
+
+        for i in 0..row_number {
+            for j in 0..col_number {
+                let pt = Point::new(j, i);
+                let hsv_values = img_hsv.at_pt::<Vec3b>(pt).unwrap();
+                
+                match hsv_values.cmp(&unvalid_hsv_value) {
+                    std::cmp::Ordering::Equal => {},
+                    _ => {
+                        n_valid_pt += 1;
+                        h_value_sum += hsv_values[0] as i32;
+                        s_value_sum += hsv_values[1] as i32;
+                        v_value_sum += hsv_values[2] as i32;
+                    }
+                }
+                
+            }
+        }
+
+        let mut mean_hsv = (0, 0, 0);
+        if n_valid_pt != 0 {
+            mean_hsv = (h_value_sum / n_valid_pt, 
+                        s_value_sum / n_valid_pt, 
+                        v_value_sum / n_valid_pt)
+        }
+
+        mean_hsv_vec.push(mean_hsv);
+        
+    }
+
+    mean_hsv_vec
 }
--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,38 @@ use color_classifier::core::rec_color;
 fn main() {
     let mut yolo = DetModule::new();
     yolo.load_model("yolov8n.pt");
-    rec_color::det_color(yolo, "/home/cu/ColorClassifier/car.jpg");
+    rec_color::color_extraction(yolo, "/home/cu/ColorClassifier/car.jpg");
 }
 
 #[cfg(test)]
 mod test {
 
     use super::*;
+    use opencv::core::Mat;
+    use opencv::core::Vector;
     use opencv::imgcodecs::{imread, imwrite, IMREAD_COLOR};
+
+    fn save_img(img_vec: Vec<Mat>, save_suffix: &str) {
+        for (i, img) in img_vec.iter().enumerate() {
+            let params = Vector::new();
+            let filename = format!("{}_{}.jpg", save_suffix, i);
+            imwrite(filename.as_str(), &img, &params).expect("保存图像失败");
+        }
+    }
 
     #[test]
     fn test_segmentation() {
         let img_roi = imread("img/test.png", IMREAD_COLOR).expect("获取图像错误");
         let img_roi_vec = vec![img_roi];
-        rec_color::segmentation(img_roi_vec);
+        let seg_img = rec_color::segmentation(img_roi_vec);
+        save_img(seg_img, "seg");
+    }
+
+    #[test]
+    fn test_cal_mean_value_of_hsv() {
+        let img_roi = imread("img/seg.jpg", IMREAD_COLOR).expect("获取图像错误");
+        let img_roi_vec = vec![img_roi];
+        rec_color::cal_mean_value_of_hsv(img_roi_vec);
     }
 
 }
